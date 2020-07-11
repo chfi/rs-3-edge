1947e0b8150f98b9a12fc72a9e26eb341c379a47
Christian <christian@chfi.se>
2020-07-10 21:21:53 -0500

    Parse input

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
-use gfa::gfa::GFA;
-use gfa::parser::parse_gfa;
-
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 use std::path::PathBuf;
 
-// struct
+type AdjacencyList = Vec<usize>;
 
-fn parse_input(path: &PathBuf) -> Vec<String> {
+fn parse_input(path: &PathBuf) -> Vec<AdjacencyList> {
     let file = File::open(path).unwrap();
-    let lines = BufReader::new(file).lines();
+    let mut result = Vec::new();
+    for line in BufReader::new(file).lines() {
+        let line = line.unwrap();
+        let fields = line.split_terminator('>');
+        let mut list = Vec::new();
+        for f in fields {
+            list.push(f.parse().unwrap());
+        }
+        result.push(list);
+    }
+    result
 }
 
 fn main() {
     let args: Vec<_> = env::args().collect();
 
-    // let num_vertices =
-    // let next_sigma_elements =
-    println!("Hello, world!");
+    let path = PathBuf::from(&args[1]);
+    let adj = parse_input(&path);
+    println!("{}", adj.len());
 }
