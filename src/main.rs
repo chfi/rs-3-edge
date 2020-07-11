44c91b3cd3b49f9cdf9276d01906e834840c5a9f
Christian <christian@chfi.se>
2020-07-11 17:24:33 -0500

    Correctly parse input, graph as BTreeMap of Vec adjacencylists

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,24 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::PathBuf;
 
+use std::collections::BTreeMap;
+
 type AdjacencyList = Vec<usize>;
 
-fn parse_input(path: &PathBuf) -> Vec<AdjacencyList> {
+type Graph = BTreeMap<usize, AdjacencyList>;
+
+fn parse_input(path: &PathBuf) -> Graph {
     let file = File::open(path).unwrap();
-    let mut result = Vec::new();
+    let mut result = BTreeMap::new();
     for line in BufReader::new(file).lines() {
         let line = line.unwrap();
-        let fields = line.split_terminator('>');
-        let mut list = Vec::new();
+        let mut fields = line.split_terminator('>');
+        let node = fields.next().and_then(|f| f.parse().ok()).unwrap();
+        let mut list: AdjacencyList = Vec::new();
         for f in fields {
             list.push(f.parse().unwrap());
         }
-        result.push(list);
+        result.insert(node, list);
     }
     result
 }
@@ -25,6 +30,12 @@ fn main() {
     let args: Vec<_> = env::args().collect();
 
     let path = PathBuf::from(&args[1]);
-    let adj = parse_input(&path);
-    println!("{}", adj.len());
+    let graph = parse_input(&path);
+    for (k, l) in graph.iter() {
+        println!("node {}", k);
+        for n in l.iter() {
+            print!("{}>", n);
+        }
+        println!();
+    }
 }
