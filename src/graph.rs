e7505629b1523db94220a51e6f7fbdf84920df7a
Christian <christian@chfi.se>
2020-07-20 23:13:44 -0500

    StructOpt CLI, support files and stdin/stdout

--- src/graph.rs
+++ src/graph.rs
@@ -1,9 +1,6 @@
 use std::collections::BTreeMap;
 use std::collections::HashMap;
-use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
-use std::path::PathBuf;
 
 use gfa::gfa::GFAParsingConfig;
 use gfa::parser::parse_gfa_stream_config;
@@ -12,8 +9,7 @@ pub type AdjacencyList = Vec<usize>;
 pub type BTreeGraph = BTreeMap<usize, AdjacencyList>;
 
 /// An adjacency list representation of a GFA graph, including the
-/// maps required to go from GFA segment names to graph node indices,
-/// and back
+/// map required to go from node index to GFA segment name
 pub struct Graph {
     pub graph: BTreeGraph,
     pub inv_names: Vec<String>,
@@ -21,52 +17,10 @@ pub struct Graph {
 
 impl Graph {
     /// Constructs an adjacency list representation of the given GFA
-    /// parser stream. Returns both the adjacency list and a map from GFA
-    /// segment names to corresponding index in the graph.
+    /// input stream, parsing the GFA line-by-line and only keeping
+    /// the links. Returns the graph as an adjacency list and a map
+    /// from graph indices to GFA segment names.
     pub fn from_gfa_reader<T: BufRead>(reader: &mut T) -> Graph {
-        let lines = &mut reader.lines();
-
-        let conf = GFAParsingConfig {
-            links: true,
-            ..GFAParsingConfig::none()
-        };
-        let gfa_lines = parse_gfa_stream_config(lines, conf);
-
-        let mut graph: BTreeMap<usize, AdjacencyList> = BTreeMap::new();
-        let mut name_map = HashMap::new();
-        let mut inv_names = Vec::new();
-
-        let mut get_ix = |name: &str| {
-            if let Some(ix) = name_map.get(name) {
-                *ix
-            } else {
-                let ix = name_map.len();
-                name_map.insert(name.to_string(), ix);
-                inv_names.push(name.to_string());
-                ix
-            }
-        };
-
-        use gfa::gfa::Line;
-        for line in gfa_lines {
-            if let Line::Link(link) = line {
-                let from_ix = get_ix(&link.from_segment);
-                let to_ix = get_ix(&link.to_segment);
-
-                graph.entry(from_ix).or_default().push(to_ix);
-                graph.entry(to_ix).or_default().push(from_ix);
-            }
-        }
-
-        Graph { graph, inv_names }
-    }
-
-    /// Constructs an adjacency list representation of the given GFA
-    /// parser stream. Returns both the adjacency list and a map from GFA
-    /// segment names to corresponding index in the graph.
-    pub fn from_gfa_file(path: &PathBuf) -> Graph {
-        let buffer = File::open(&path).unwrap();
-        let reader = BufReader::new(buffer);
         let lines = &mut reader.lines();
 
         let conf = GFAParsingConfig {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::env;
-use std::io::Write;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{BufReader, BufWriter, Write};
 use std::path::PathBuf;
 
 use structopt::StructOpt;
@@ -8,8 +9,27 @@ use three_edge_connected::algorithm;
 use three_edge_connected::graph::Graph;
 use three_edge_connected::state::State;
 
+/// Finds the 3-edge-connected components in a graph. Input must be a
+/// bridgeless graph in the GFA format. Output is a list of
+/// 3-edge-connected components, one per line, as space-delimited
+/// lists of segment names.
+#[derive(StructOpt, Debug)]
+struct Opt {
+    /// If true, read input GFA on stdin.
+    #[structopt(short, required_unless("in-file"))]
+    stdin: bool,
+
+    /// GFA file to use, must be present if not reading from stdin.
+    #[structopt(short, long, parse(from_os_str), required_unless("stdin"))]
+    in_file: Option<PathBuf>,
+
+    /// Output file. If empty, writes on stdout.
+    #[structopt(short, long, parse(from_os_str))]
+    out_file: Option<PathBuf>,
+}
+
 /// Prints each component, one per row, with space-delimited GFA
-/// segment names, in the BTreeMap node order
+/// segment names, in the node index order
 fn write_components<T: Write>(
     stream: &mut T,
     inv_names: &[String],
@@ -28,14 +48,37 @@ fn write_components<T: Write>(
 }
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    let path = PathBuf::from(&args[1]);
-    let algraph = ALGraph::from_gfa_file(&path);
+    let opt = Opt::from_args();
 
-    let mut state = State::initialize(&algraph.graph);
+    let mut in_handle: Box<dyn BufRead> = {
+        match opt.in_file {
+            None => Box::new(BufReader::new(std::io::stdin())),
+            Some(path) => {
+                let file = File::open(&path).unwrap_or_else(|_| {
+                    panic!("Could not open file {:?}", path)
+                });
+                Box::new(BufReader::new(file))
+            }
+        }
+    };
 
-    algorithm::three_edge_connect(&algraph.graph, &mut state);
+    let graph = Graph::from_gfa_reader(&mut in_handle);
 
-    let mut stdo = std::io::stdout();
-    write_components(&mut stdo, &algraph.inv_names, state.components());
+    let mut state = State::initialize(&graph.graph);
+
+    algorithm::three_edge_connect(&graph.graph, &mut state);
+
+    let mut out_handle: Box<dyn Write> = {
+        match opt.out_file {
+            None => Box::new(BufWriter::new(std::io::stdout())),
+            Some(path) => {
+                let fout = File::create(&path).unwrap_or_else(|_| {
+                    panic!("Could not create file {:?}", path)
+                });
+                Box::new(BufWriter::new(fout))
+            }
+        }
+    };
+
+    write_components(&mut out_handle, &graph.inv_names, state.components());
 }
